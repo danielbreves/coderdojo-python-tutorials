0f01a896ba607140bbe035c5cf313c53472e7f22
Daniel Ribeiro <djbreves@gmail.com>
2026-03-28 21:48:37 +1100

    Enhance tutorial instructions by clarifying expected output for Step 3

--- level-1.py
+++ level-1.py
@@ -17,7 +17,7 @@ Let's do this by creating a variable called "life" and setting it to 3.
 
 
 # Step 3: To run this file, type "python level-1.py" in the terminal at the bottom
-# of the screen and hit Enter.
+# of the screen and hit Enter. You should see the number 3 appear!
 
 # Step 4: Now try print("life") and print(blah) below this line.
 # What happens? Can you spot the difference?
